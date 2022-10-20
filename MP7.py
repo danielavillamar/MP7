1ba7dc849664740f003ace1c9832c2c320275329
RobertAlexCL <rsstocast@gmail.com>
2022-10-20 20:51:29 +0200

    ball and player with collisions

--- MP7.py
+++ MP7.py
@@ -7,11 +7,18 @@
 
 # Importamos pygame 
 import pygame
+import random
 # Definición de pantalla del simulador
 pixel_len = 12
 height_pixels = 65
 width_pixels = 60
 width, height = width_pixels*pixel_len, height_pixels*pixel_len
+
+# Colres del jugador y la pelota
+player_color = (0,0,0) # Black
+ball_color = (0, 0, 0) # Black
+
+
 
 # Creación de la cancha
 def createHorizontalLine(xo, xf, y):
@@ -25,6 +32,25 @@ class Simulation(object):
     self.screen = screen
     self.modx = width 
     self.mody = height
+
+    # Coordenadas del jugador
+    self.player_x = 30
+    self.player_y = 30
+
+    # Stats del jugador
+    self.player_width = 20
+    self.player_height = 30
+    self.player_current_direction = (0,0)
+
+    # Coordenadas de la pelota
+    self.ball_x = 100
+    self.ball_y = 100
+
+    # Stats de la pelota
+    self.ball_width = 10
+    self.ball_velocity = 20
+    self.ball = pygame.draw.circle(screen, ball_color, (self.ball_x, self.ball_y), self.ball_width)
+    self.player = pygame.draw.rect(screen, player_color, (self.player_x, self.player_y, self.player_width, self.player_height))
 
     for point in initial:
       self.paint(point[0], point[1])
@@ -49,6 +75,48 @@ class Simulation(object):
   def moveUp(self, x, y):
     pass
 
+  # La dirección tiene que ser un vector unitario
+  def ball_interactions(self, direction, strength): 
+    direction_x = direction[0]
+    direction_y = direction[1]
+
+    # derecha
+    if direction_x == 1: 
+      if self.ball_x + strength > self.win_width:
+        self.ball_interactions((-direction_x, 0), strength) 
+      else:
+        self.ball_x += strength
+
+    # izquierda
+    if direction_x == -1:  
+      if self.ball_x - strength < 0:
+        self.ball_interactions((-direction_x, 0), strength) 
+      else:
+        self.ball_x -= strength
+    
+    # abajo
+    if direction_y == 1:
+      if self.ball_y + strength > self.win_height:
+        self.ball_interactions((0, -direction_y), strength) 
+      else:
+        self.ball_y += strength
+    
+    # arriba
+    if direction_y == -1:  
+      if self.ball_y - strength < 0:
+        self.ball_interactions((0, -direction_y), strength) 
+      else:
+        self.ball_y -= strength
+
+  # Funcion para detectar colisiones
+  def collisions(self):
+    if self.ball.colliderect(self.player):
+      self.playerInteraction()
+
+  # Interacción con el jugador
+  def playerInteraction(self):
+    self.ball_interactions(self.player_current_direction, self.ball_velocity * 2) 
+
 # START
 pygame.init()
 screen = pygame.display.set_mode((width, height))
@@ -69,7 +137,9 @@ simulation = Simulation(
   initial=lines,
 )
 
-ITERATIONS = 100
+
+
+ITERATIONS = 1000
 for _ in range(ITERATIONS):
   pygame.time.delay(10)
   pygame.display.flip()
